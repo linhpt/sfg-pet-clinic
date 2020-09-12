2dd81a205d215439b0d255c0c2bf0343e97d95f5
LinhPT <linhpt.uet@gmail.com>
2020-09-12 23:50:15 +0700

    added pets and contact info to owner on startup

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -9,28 +9,29 @@ import java.util.Set;
 
 @Service
 public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
     @Override
     public Set<PetType> findAll() {
-        return null;
-    }
-
-    @Override
-    public void deleteById(Long id) {
-
-    }
-
-    @Override
-    public void delete(PetType object) {
-
-    }
-
-    @Override
-    public PetType save(PetType object) {
-        return null;
+        return super.findAll();
     }
 
     @Override
     public PetType findById(Long id) {
-        return null;
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
     }
 }
