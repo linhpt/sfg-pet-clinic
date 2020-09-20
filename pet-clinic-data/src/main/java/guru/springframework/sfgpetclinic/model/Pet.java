2b8c55b079aa39694696e76fd6b4e28f7faf3428
LinhPT <linhpt.uet@gmail.com>
2020-09-20 16:51:00 +0700

    create entity Visit

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 /**
  * Created by jt on 7/13/18.
@@ -12,15 +13,20 @@ public class Pet extends BaseEntity{
 
     @Column(name = "name")
     private String name;
+
     @ManyToOne
     @JoinColumn(name = "type_id")
     private PetType petType;
+
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits;
 
     public String getName() {
         return name;
